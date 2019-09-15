4b919588523d0687192e92c695962bcff7d38630
fareseldiren <54963914+fareseldiren@users.noreply.github.com>
2019-09-15 23:38:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`LXRD DV IGNXRED`,"http://twitch.tv/fares")
+client.user.setGame(`متساليش عليا `,"http://twitch.tv/fares")
 client.user.setStatus("dnd")
 });
 
